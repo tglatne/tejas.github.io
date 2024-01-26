de7d882c564f053d9cbb8894762efef1f63bce9b
Tejas <tejas0097@gmail.com>
2024-01-26 16:52:37 -0500

    added missing files related to exp section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import AboutSection from "@/components/AboutSection";
-import HeroSection from "@/components/HeroSection";
-import ProjectsSection from "@/components/ProjectsSection";
+import AboutSection from '@/components/AboutSection';
+import HeroSection from '@/components/HeroSection';
+import ProjectsSection from '@/components/ProjectsSection';
+import ExperienceSection from '@/components/ExpSection';
 export default function Home() {
   return (
-    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+    <main className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl'>
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
