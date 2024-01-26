0b3810c238c7ea677d9efa325aeb7af639445148
Tejas <tejas0097@gmail.com>
2024-01-26 17:16:59 -0500

    check exp section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import AboutSection from '@/components/AboutSection';
 import HeroSection from '@/components/HeroSection';
 import ProjectsSection from '@/components/ProjectsSection';
-import ExperienceSection from '@/components/ExpSection';
+import ExperienceSection from '@/components/ExperienceSection';
 export default function Home() {
   return (
     <main className='mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl'>
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
-      <ExperienceSection />
+      {/* <ExperienceSection /> */}
     </main>
   );
 }
