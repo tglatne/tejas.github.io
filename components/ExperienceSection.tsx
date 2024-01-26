c11c2a63da95dcb8fe0b9bdad686f8b9722e0a43
Tejas <tejas0097@gmail.com>
2024-01-26 18:37:07 -0500

    designing exp section2

--- components/ExperienceSection.tsx
+++ components/ExperienceSection.tsx
@@ -11,50 +11,45 @@ import {
 
 const ExperienceSection = () => {
   return (
-    // <Card>
-    //   <CardContent>
-    //     <Typography variant='h4' gutterBottom>
-    //       Professional Experience
-    //     </Typography>
+    <Card>
+      <CardContent>
+        <Typography variant='h4' gutterBottom>
+          Professional Experience
+        </Typography>
 
-    //     <Timeline>
-    //       {/* Example Timeline Item 1 */}
-    //       <TimelineItem>
-    //         <TimelineSeparator>
-    //           <TimelineDot />
-    //           <TimelineConnector />
-    //         </TimelineSeparator>
-    //         <TimelineContent>
-    //           <Typography variant='h6'>Company Name</Typography>
-    //           <Typography variant='subtitle1'>Job Title</Typography>
-    //           <Typography variant='body2'>Date Range</Typography>
-    //           <Typography variant='body1'>
-    //             Description of your responsibilities and achievements.
-    //           </Typography>
-    //         </TimelineContent>
-    //       </TimelineItem>
+        {/* <Timeline>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography variant='h6'>Company Name</Typography>
+              <Typography variant='subtitle1'>Job Title</Typography>
+              <Typography variant='body2'>Date Range</Typography>
+              <Typography variant='body1'>
+                Description of your responsibilities and achievements.
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
 
-    //       {/* Example Timeline Item 2 */}
-    //       <TimelineItem>
-    //         <TimelineSeparator>
-    //           <TimelineDot />
-    //         </TimelineSeparator>
-    //         <TimelineContent>
-    //           <Typography variant='h6'>Another Company</Typography>
-    //           <Typography variant='subtitle1'>Another Job Title</Typography>
-    //           <Typography variant='body2'>Another Date Range</Typography>
-    //           <Typography variant='body1'>
-    //             Another set of responsibilities and achievements.
-    //           </Typography>
-    //         </TimelineContent>
-    //       </TimelineItem>
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography variant='h6'>Another Company</Typography>
+              <Typography variant='subtitle1'>Another Job Title</Typography>
+              <Typography variant='body2'>Another Date Range</Typography>
+              <Typography variant='body1'>
+                Another set of responsibilities and achievements.
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
 
-    //       {/* Add more TimelineItems as needed */}
-    //     </Timeline>
-    //   </CardContent>
-    // </Card>
-    // );
-    <div>hello</div>
+        </Timeline> */}
+      </CardContent>
+    </Card>
   );
 };
 
