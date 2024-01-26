0a2522c31b453b84181f9f2d14386d0cd0707bac
Tejas <tejas0097@gmail.com>
2024-01-26 16:50:24 -0500

    added exp section

---
+++ components/ExpSection.tsx
@@ -0,0 +1,60 @@
+// ExperienceSection.js
+import React from 'react';
+import { Card, CardContent, Typography } from '@mui/material';
+import {
+  Timeline,
+  TimelineItem,
+  TimelineSeparator,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+} from '@mui/lab';
+
+const ExperienceSection = () => {
+  return (
+    <Card>
+      <CardContent>
+        <Typography variant='h4' gutterBottom>
+          Professional Experience
+        </Typography>
+
+        <Timeline>
+          {/* Example Timeline Item 1 */}
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography variant='h6'>Company Name</Typography>
+              <Typography variant='subtitle1'>Job Title</Typography>
+              <Typography variant='body2'>Date Range</Typography>
+              <Typography variant='body1'>
+                Description of your responsibilities and achievements.
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+
+          {/* Example Timeline Item 2 */}
+          <TimelineItem>
+            <TimelineSeparator>
+              <TimelineDot />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Typography variant='h6'>Another Company</Typography>
+              <Typography variant='subtitle1'>Another Job Title</Typography>
+              <Typography variant='body2'>Another Date Range</Typography>
+              <Typography variant='body1'>
+                Another set of responsibilities and achievements.
+              </Typography>
+            </TimelineContent>
+          </TimelineItem>
+
+          {/* Add more TimelineItems as needed */}
+        </Timeline>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ExperienceSection;
