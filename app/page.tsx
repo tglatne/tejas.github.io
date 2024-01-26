001e3b86652b4908009ef322eb25a6485b91a3a8
Tejas <tejas0097@gmail.com>
2024-01-26 17:28:04 -0500

    exp section added

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
-      {/* <ExperienceSection /> */}
+      <ExperienceSection />
     </main>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
       <div className='justify-between md:items-center md:flex'>
         <div>
           <div className='flex items-center justify-between py-3 md:py-5 md:block'>
-            <Link to='/home'>
+            <Link to='home' href='home'>
               <div className='container flex items-center space-x-2'>
                 <h2 className='text-2xl text-orange-700 font-bold'>
                   TEJAS LATNE
